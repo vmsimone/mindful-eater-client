4490608c2357812179745099e6d744af1133735b
Vincent Simone <vsimone@protonmail.com>
2018-08-01 23:59:28 -0700

    fixed tests

--- src/components/greeting.test.js
+++ src/components/greeting.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Greeting from './greeting.js';
+import {Greeting} from './greeting.js';
 
 describe('<Greeting />', () => {
     it('should render', () => {
-        shallow(<Greeting />);
+        shallow(<Greeting username="Demo" />);
     });
 });
